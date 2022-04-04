7b738846410a8d569421bf8b113c11835afc9f87
Peter Stegger <stegger.peter@gmail.com>
2022-04-04 11:13:05 +0200

    First

---
+++ Examples/Encryption/PasswordToKeyService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Examples.Encryption
+{
+    public class PasswordToKeyService
+    {
+        public byte[] GetKey(string password, out byte[] salt, int keyLength)
+        {
+            if (keyLength % 8 != 0)
+                throw new ArgumentException("Key length must be a multiple of 8");
+
+            using (var derivedBytes = new Rfc2898DeriveBytes(password, keyLength))
+            {
+                salt = derivedBytes.Salt;
+                return derivedBytes.GetBytes(keyLength / 8);
+            }
+        }
+
+        public byte[] GetKey(string password, byte[] salt, int keyLength)
+        {
+            if (keyLength % 8 != 0)
+                throw new ArgumentException("Key length must be a multiple of 8");
+
+            using (var derivedBytes = new Rfc2898DeriveBytes(password, salt))
+            {
+                return derivedBytes.GetBytes(keyLength / 8);
+            }
+        }
+    }
+}
---
+++ Examples/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.SimpleHashExample();
+            
+            Console.WriteLine("");
+            
+            program.TegridyHash();
+        }
+
+        private void SimpleHashExample()
+        {
+            string input = "Hello World!";
+
+            HashAlgorithm sha = SHA256.Create();
+            byte[] bytes = sha.ComputeHash(UTF8Encoding.UTF8.GetBytes(input));
+
+            string hash = HashBytesToString(bytes);
+
+            Console.WriteLine(hash);
+        }
+
+        private static string HashBytesToString(byte[] bytes)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+
+            string hash = builder.ToString();
+            return hash;
+        }
+
+        private void TegridyHash()
+        {
+            byte[] tegridyA = File.ReadAllBytes("C:/Users/stegg/Desktop/Workspace/Tegridy_A.zip");
+            byte[] tegridyB = File.ReadAllBytes("C:/Users/stegg/Desktop/Workspace/Tegridy_B.zip");
+            
+            HashAlgorithm sha = SHA256.Create();
+            
+            byte[] bytesHashA = sha.ComputeHash(tegridyA);
+            byte[] bytesHashB = sha.ComputeHash(tegridyB);
+
+            string hashA = HashBytesToString(bytesHashA);
+            string hashB = HashBytesToString(bytesHashB);
+            
+            Console.WriteLine("Tegridy_A: " + hashA);
+            Console.WriteLine("Tegridy_B: " + hashB);
+            
+        }
+        
+        
+    }
+}
